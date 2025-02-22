ee97a3b803746c5bcb7a9ac423030d280ee78740
Fra <fra24eso@gmail.com>
2025-02-23 00:41:48 +0100

    vue-router@4

---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,31 @@
+<script>
+import ColorMode from "./ColorMode.vue";
+
+
+export default {
+    name: "Navbar",
+    components: {
+        ColorMode,
+    }
+};
+</script>
+
+<template>
+    <nav class="navbar justify-content-center">
+        <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <!-- <li><router-link to="/data">Data</router-link></li> -->
+            <li>
+                <ColorMode />
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
